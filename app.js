4c5475aaac88ef50b0f4cc9f1f19138c299ef39d
Manish Mewada <manishmewada2002@gmail.com>
2022-11-22 20:13:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,7 +10,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://Chat-App:chattomanish@cluster0.6tls3yu.mongodb.net/todoList?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.use(express.json());
 
 const itemsSchema = {
   name: String
@@ -77,6 +83,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(3000, ()=> {
   console.log("Server started on port 3000");
 });
